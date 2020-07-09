f548be2724ea6ca1cabdbcf4123caf5c34b78128
DuyNV <duynv@DuyNVs-MacBook-Pro.local>
2020-07-09 17:10:07 +0700

    Update status HSPhieukham

--- api/models/HSPhieukham.js
+++ api/models/HSPhieukham.js
@@ -9,8 +9,78 @@ const phieukhamSchema = new Schema({
         required: true
     },
     bacSyKham: {
-
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+    },
+    MaPhieuKham: {
+        type: String,
+        required: true
+    },
+    GioBatDauKham: {
+        type: Date,
+    },
+    GioKetThucKham: {
+        type: Date,
+    },
+    DienBienBenh: {
+        type: String
+    },
+    TienSuBenh: {
+        type: String
+    },
+    ToanThan: {
+        type: String
+    },
+    CacBoPhan: {
+        type: String
+    },
+    ChanDoan: {
+        type: String
+    },
+    ToanThan: {
+        type: String
+    },
+    PPDieuTri: {
+        type: String
+    },
+    Mach: {
+        type: String
+    },
+    NhietDo: {
+        type: String
+    },
+    HuyetApTren: {
+        type: String
+    },
+    HuyetApDuoi: {
+        type: String
+    },
+    NhipTho: {
+        type: String
+    },
+    SPO2: {
+        type: String
+    },
+    CanNang: {
+        type: String
+    },
+    ChieuCao: {
+        type: String
+    },
+    TrangThai: {
+        type: Number,
+        required: true
+            // định nghĩa trạng thái: 
+            // 1 - vừa đón tiếp xong đồng thời có ở danh sách chờ khám, 
+            // 2 - có tên ở danh sách đang khám
+            // 3 - có tên ở danh sách đã khám xong
+            // 4 - đã huỷ khám
+            // 5 - có tên ở danh sách chuyển viện
+            // 6 - có tên ở danh sách chờ cấp số vào viện
+            // 7 - có tên ở danh sách đang điều trị nội trú
+            // 8- có tên ở danh sách đã ra viện
     }
+
 
 });
 
